25ae9215660989f40a0d11aa839c40239a63d4c3
ukae <k.ueta@aihub.tokyo>
2024-12-26 21:50:35 +0900

    Update pages

--- app/page.tsx
+++ app/page.tsx
@@ -15,21 +15,32 @@ export default function Home() {
   
 
   const handleVideoEnd = () => {
-    console.log("動画再生が終了しました"); 
-    router.push("/result"); // 結果ページへ遷移
+    console.log("動画再生が終了しました"); // デバッグログ
+    try {
+      router.push("/result"); // 結果ページへ遷移
+      console.log("ルート遷移が成功しました");
+    } catch (error) {
+      console.error("ルート遷移に失敗しました", error);
+    }
   };
 
   return (
+    
     <div
       className="h-screen flex items-center justify-center bg-cover bg-center relative"
-      style={{ backgroundImage: "url('/static/background/index.png')" }}
+      style={{
+        backgroundImage: "url('/static/background/index.png')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
     >
+
       <div className="absolute inset-0 bg-black bg-opacity-50"></div>
       <div className="text-center z-10">
         {showButton && (
           <button
             onClick={handleButtonClick}
-            className="magic-hover magic-fade bg-gradient-to-r from-pink-500 to-purple-500 text-white text-xl px-8 py-4 rounded-full shadow-lg hover:scale-110 transition-transform"
+            className="magic-hover magic-fade bg-gradient-to-r from-pink-500 to-purple-500 text-white text-xl sm:text-2xl md:text-3xl px-6 sm:px-8 py-3 sm:py-4 rounded-full shadow-lg hover:scale-110 transition-transform"
           >
             おみくじを引く
           </button>
@@ -38,7 +49,7 @@ export default function Home() {
           <video
             src="/static/video/load.mp4"
             autoPlay
-            className="w-full h-full object-cover"
+            className="w-full h-full object-cover md:max-h-screen md:aspect-video"
             onEnded={handleVideoEnd}
           />
         )}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        adobe: ['fot-tsukuaoldmin-pr6n','sans-serif']
+      }
+    },
   },
   plugins: [],
-}
-
+};
