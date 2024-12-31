5c67a9f4521983bce5a4260e6c1d720cc17fe1fe
ukae <k.ueta@aihub.tokyo>
2024-12-31 23:09:22 +0900

    perfect

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { AnimatePresence } from "framer-motion";
+import { Noto_Sans_JP } from 'next/font/google'
+
+const notoSansJP = Noto_Sans_JP({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://new-year-new-me-six.vercel.app/'),
@@ -8,8 +11,6 @@ export const metadata: Metadata = {
   description: "デジタルから離れて、自分を見つめ直す時間を少しだけ作るおみくじアプリ",
   icons: {
     icon: '/static/icons/favicon.png',
-    shortcut: '/static/icons/favicon.png',
-    apple: '/static/icons/favicon.png',
   },
   openGraph: {
     title: "NewYear NewMe",
@@ -19,12 +20,8 @@ export const metadata: Metadata = {
         url: "/static/background/OGP.png",
         width: 1200,
         height: 630,
-        alt: "NewYear NewMe",
       },
     ],
-    url: 'https://new-year-new-me-six.vercel.app/',
-    locale: "ja_JP",
-    type: "website",
   },
   twitter: {
     card: "summary_large_image",
